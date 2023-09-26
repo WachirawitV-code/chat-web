0d8094855ec8f4dfb1c4217f621e060f0708d7d0
Wachirawit <wachirawit@senestia.com>
2023-09-26 09:50:27 +0700

    chat UI

--- chat-web/src/app/[name]/chat/layout.tsx
+++ chat-web/src/app/[name]/chat/layout.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 const chat = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+        <main>{children}</main>
+  );
 };
 
 export default chat;
